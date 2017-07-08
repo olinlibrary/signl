b24d2e05a2d45b240e85bc7159152c7679d90cc0
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2017-07-08 19:48:59 -0400

    First pass at sign using rune font

--- sketch.js
+++ sketch.js
@@ -1,16 +1,43 @@
+var palette = [
+  '#000000',
+  '#A7A9AC',
+  '#ED037C',
+  '#009BDF',
+  '#26AAA5',
+  '#349E49',
+  '#F47920',
+]
+
+var margin = 20
+var padding = 10
+var textHeight = 50
+var textWidth = 565
+
+
 var r = new Rune({
   container: "body",
-  width: 600,
-  height: 400,
+  width: textWidth + (margin + padding) * 2,
+  height: textHeight + (margin + padding) * 2,
+  // background: ''
   debug: true,
 });
 
 
-var container = r.polygon(0, 0)
+var container = r.polygon(margin, margin)
   .lineTo(0, 0)
-  .lineTo(100, 0)
-  .lineTo(100, 50)
-  .lineTo(0, 50)
+  .lineTo(textWidth + padding * 2, 0)
+  .lineTo(textWidth + padding * 2, textHeight + padding * 2)
+  .lineTo(0, textHeight + padding * 2)
+  .fill(palette[Math.floor(Math.random() * palette.length)])
+  .stroke(false)
   // automatically closes
+
+var text = r.text('SOFTWARE OF SUMMER', margin + padding, margin + padding + textHeight)
+  .fontFamily('DINOT')
+  .fontSize('70px')
+  .fill(255, 255, 255)
+  .stroke(false)
+
+// r.width(600)
 
 r.draw();
