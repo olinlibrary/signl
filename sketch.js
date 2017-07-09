44eb49fdb50bb8b12785c138d08db1c344358ad4
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2017-07-09 01:23:41 -0400

    Switch to Paper.js

--- sketch.js
+++ sketch.js
@@ -6,38 +6,47 @@ var palette = [
   '#26AAA5',
   '#349E49',
   '#F47920',
-]
+];
 
-var margin = 20
-var padding = 10
-var textHeight = 50
-var textWidth = 565
+var msg = "SOFTWARE OF SUMMER";
 
+var DEBUG = false;  // show bounding lines
 
-var r = new Rune({
-  container: "body",
-  width: textWidth + (margin + padding) * 2,
-  height: textHeight + (margin + padding) * 2,
-  // background: ''
-  debug: true,
+var margin = 20;
+var padding = 10;
+
+font_size = 50;
+
+var anchor = new Point(margin, margin);
+var text = new PointText({
+  content: 'SOFTWARE OF SUMMER',
+  fillColor: '#FFF',
+  fontFamily: 'DINOT',
+  fontSize: font_size,
+  justification: 'center',
 });
 
+var textHeight = text.bounds.height * 0.6;  // account for text height funk
+var textWidth = text.bounds.width;
+var rect = new Path.Rectangle({
+  position: paper.view.center,
+  size: new Size(textWidth, textHeight) + [padding * 2, padding * 2],
+  fillColor: palette[Math.floor(Math.random() * palette.length)],
+}).insertBelow(text);
+text.point = rect.position + [0, textHeight / 2];
+project.activeLayer.skew([-20, 0]);  // skew everything 20 degrees
 
-var container = r.polygon(margin, margin)
-  .lineTo(0, 0)
-  .lineTo(textWidth + padding * 2, 0)
-  .lineTo(textWidth + padding * 2, textHeight + padding * 2)
-  .lineTo(0, textHeight + padding * 2)
-  .fill(palette[Math.floor(Math.random() * palette.length)])
-  .stroke(false)
-  // automatically closes
+// resize canvas to fit borders + margin and reposition sign to center
+paper.view.viewSize.width = rect.bounds.width + margin * 2;
+paper.view.viewSize.height = rect.bounds.height + margin * 2;
+project.activeLayer.position = paper.view.center;
 
-var text = r.text('SOFTWARE OF SUMMER', margin + padding, margin + padding + textHeight)
-  .fontFamily('DINOT')
-  .fontSize('70px')
-  .fill(255, 255, 255)
-  .stroke(false)
-
-// r.width(600)
-
-r.draw();
+if (DEBUG) {  // show bounding lines
+  text.selected = true;
+  Path.Rectangle({
+    position: rect.position,
+    size: new Size(textWidth, textHeight),
+    selected: true,
+  });
+  rect.position.selected = true;
+};
